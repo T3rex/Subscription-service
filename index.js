58be317ae45f963742e998d7ad44aa8d62f96f68
T3rex <31337aditya@gmail.com>
2023-08-06 01:12:54 +0530

    subsciption route added

--- index.js
+++ index.js
@@ -26,16 +26,32 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  plan: String,
+  interval: String
 });
 const User = mongoose.model('User', userSchema);
 
+const userAuthenticate = async (req,res,next)=>{
+  try{    
+    const user = await User.findOne({email:req.body.email, password:req.body.password})   
+    if(user){
+      req.user = user;
+      next();
+    }
+    else{
+      res.status(500).json({msg: 'Login failed'});
+    }
+  }catch(err){
+
+  }
+}
 
 app.post('/signup', (req, res) => {
   try{
     const name = req.body.name;
     const email = req.body.email;
-    const pass = req.body.password;
-    const user = new User({ name: name, email: email, password: pass});
+    const password = req.body.password;
+    const user = new User({ name: name, email: email, password: password});
     user.save();
     res.status(201).json({success: true, msg: "User created successfully"});
   }
@@ -43,6 +59,27 @@ app.post('/signup', (req, res) => {
     res.status(500).json({success: false, err: err});
   }
 });
+
+app.post('/login',userAuthenticate,(req,res)=>{
+  try{
+    res.status(200).json({success:true,msg:"User authenticated"});
+  }catch(err){
+     res.status(500).json({success: false, err: err});
+  }
+})
+
+app.post('/subscribe',userAuthenticate,async(req,res)=>{
+  try{   
+    req.user.plan = req.body.plan;;
+    req.user.interval = req.body.interval;    
+    const user = await User.findByIdAndUpdate(req.user._id,req.user);
+    res.status(200).json({success:true,msg:"Subcription successful"});
+  }catch(err){
+
+     res.status(500).json({success: false, err: err});
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}`)
